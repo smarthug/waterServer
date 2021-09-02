2c4753cdd3ba768b3116e72c2639ac78efd123c0
hosuk <kirklayer@gmail.com>
2021-09-02 11:34:35 +0900

    test done

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const cors = require('cors');
 const Axios = require('axios')
 const app = express()
 const port = 8000;
-const serviceKey = "24%2Bx51XF5tzWd2BWHdjkll0xieI5fwBw3EDInWOps9zYyGjIr3hPwKCrawhuPOMHlZ3iYvc0eWrMlTYqTD2jQA%3D%3D"
+// 지급 받은 url
+// const serviceKey = "24%2Bx51XF5tzWd2BWHdjkll0xieI5fwBw3EDInWOps9zYyGjIr3hPwKCrawhuPOMHlZ3iYvc0eWrMlTYqTD2jQA%3D%3D"
 // const serviceKey = "24+x51XF5tzWd2BWHdjkll0xieI5fwBw3EDInWOps9zYyGjIr3hPwKCrawhuPOMHlZ3iYvc0eWrMlTYqTD2jQA=="
-const url =  'http://opendata.kwater.or.kr/openapi-data/service/pubd/dam/sluicePresentCondition/mnt/list';
+// 
+const serviceKey = "ejdrD89pyah0JlAaICprH0xOAEp0tAxvExhm2p0DT5Ulq2MskjlekFH7kFIAEt6d16gjJ2scGwRSLG4Rr1HUiA=="
+const url = 'http://opendata.kwater.or.kr/openapi-data/service/pubd/dam/sluicePresentCondition/mnt/list';
+const perfecturl = 'http://opendata.kwater.or.kr/openapi-data/service/pubd/dam/sluicePresentCondition/mnt/list?damcode=2403201&stdt=2021-09-02&eddt=2021-09-02&numOfRows=10&pageNo=undefined&serviceKey=ejdrD89pyah0JlAaICprH0xOAEp0tAxvExhm2p0DT5Ulq2MskjlekFH7kFIAEt6d16gjJ2scGwRSLG4Rr1HUiA=='
 
 // let queryParams = '?' + encodeURIComponent('ServiceKey') + '='+encodeURIComponent(serviceKey); /*Service Key*/
 // let queryParams = '?' + encodeURIComponent('ServiceKey') + '='+decodeURIComponent(serviceKey); /*Service Key*/
 // let queryParams = '?' + encodeURIComponent('ServiceKey') + '='+serviceKey; /*Service Key*/
-let queryParams = '?' 
-// queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-queryParams +=  encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-queryParams += '&' + encodeURIComponent('damcode') + '=' + encodeURIComponent('2022510'); /**/
-queryParams += '&' + encodeURIComponent('stdt') + '=' + encodeURIComponent('2021-9-01'); /**/
-queryParams += '&' + encodeURIComponent('eddt') + '=' + encodeURIComponent('2021-9-01'); /**/
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-queryParams +=  '&' + encodeURIComponent('ServiceKey') + '='+serviceKey; /*Service Key*/
+let queryParams = '?'
+queryParams += encodeURIComponent('damcode') + '=' + encodeURIComponent('2403201'); /**/
+queryParams += '&' + encodeURIComponent('stdt') + '=' + encodeURIComponent('2020-09-02'); /**/
+queryParams += '&' + encodeURIComponent('eddt') + '=' + encodeURIComponent('2020-09-02'); /**/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('144'); /**/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('undefined'); /**/
+queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + serviceKey; /*Service Key*/
 
-console.log(queryParams)
+// console.log(queryParams)
 
 
 
@@ -29,11 +32,17 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     // console.log(url+queryParams)
-    Axios.get(url+queryParams).then((response)=>{
-        // console.log(res);
+    // console.log(url + queryParams)
+    // console.log(perfecturl)
+    // Axios.get(perfecturl).then((response) => {
+    //     console.log(response.data);
+    //     res.send(response.data)
+    // })
+
+    Axios.get(url + queryParams).then((response) => {
+        console.log(response.data);
         res.send(response.data)
     })
-
 })
 
 app.listen(port, () => {
